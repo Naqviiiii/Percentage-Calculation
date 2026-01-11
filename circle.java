ab185e8b66ec17a902dc7a6e83fd22c1f78c1344
Naqviiiii <syedmuhammadakber6@gmail.com>
2026-01-11 18:38:53 +0500

    Add files via upload

---
+++ circle.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class circle {
+    public static void main(String[] args){
+        Scanner input= new Scanner(System.in);
+        System.out.print("Enter the radius of the circle  :");
+        double Radi=input.nextDouble();
+        double Area;
+        double Circ;
+        Area=Radi*Radi*3.14159;
+        Circ=2*3.14159*Radi;
+        System.out.println("The Area of this cricle is " + Area);
+        System.out.println("The Circumference of this circle is " + Circ);
+    }
+}

b82324a1e40600aa8e9ff202dad8a6ab142d22a2
Naqviiiii <syedmuhammadakber6@gmail.com>
2026-01-11 18:21:55 +0500

    Add files via upload

---
+++ percentage.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class percentage {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        double GM ;
+        int TM;
+        double P;
+        System.out.print("Enter your obtained marks :");
+        GM = input.nextDouble();
+        System.out.print("Enter your total marks :");
+        TM = input.nextInt();
+        P = GM*100/TM;    
+        System.out.print(P+"%");
+    }
+}    
+    
+
+
